2625ee37751fa042c86992b8c3583f144b26eebb
Sai Chinmay <47316906+saichinmay2000@users.noreply.github.com>
2020-03-12 00:24:14 +0530

    Added Committee And also added GStatic Code in JS Strict Mode

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-159402006-1';
+document.head.appendChild(imported);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());
+gtag('config', 'UA-159402006-1');
